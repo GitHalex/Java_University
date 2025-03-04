38547ab45ee38f103fb89092fd30f0d124d2766a
GitHalex <calex1731@gmail.com>
2025-03-04 00:24:22 -0400

    Agregando nuevos valores

---
+++ src/com/javauniversityExamples/Book.java
@@ -0,0 +1,4 @@
+package com.javauniversityExamples;
+
+public class Book {
+}
---
+++ src/com/javauniversityExamples/Example.java
@@ -0,0 +1,8 @@
+package com.javauniversityExamples;
+
+class Example {
+    public static void main(String[] args) {
+        System.out.println("Esto es una prueba");
+    }
+
+}
