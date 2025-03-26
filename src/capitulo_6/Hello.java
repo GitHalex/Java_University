5b6e9c7a606df444c0a0676d22199640813d128e
GitHalex <calex1731@gmail.com>
2025-03-25 20:33:07 -0400

    Agregando nuevos ejercicios

--- src/capitulo_6/Hello.java
+++ src/capitulo_6/Hello.java
@@ -3,5 +3,10 @@ package capitulo_6;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("ha");
+        System.out.println("Esto");
+
+
+
     }
 }
