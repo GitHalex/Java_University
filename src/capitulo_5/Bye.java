c7560c69ff633ddfd7b3fb28e3774a31e0538f7e
GitHalex <calex1731@gmail.com>
2025-03-25 20:21:27 -0400

    Agrgando nuevos ejercicios

--- src/capitulo_5/Bye.java
+++ src/capitulo_5/Bye.java
@@ -3,5 +3,7 @@ package capitulo_5;
 public class Bye {
     public static void main(String[] args) {
         System.out.println("bye");
+        double numero = 3.5;
+        System.out.println("numero = " + numero);
     }
 }
