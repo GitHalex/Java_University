a76ba9d7de0380a5cde076a784c9e54be874f4ca
GitHalex <calex1731@gmail.com>
2025-03-27 17:43:33 -0400

    Agregando nuevso archivos

--- src/capitulo_6/Emily.java
+++ src/capitulo_6/Emily.java
@@ -2,6 +2,7 @@ package capitulo_6;
 
 public class Emily {
     public static void main(String[] args) {
+        
         System.out.print("A bird came down the walk:");
         System.out.print("He did not know I saw;");
         System.out.print("He bit an angle-worm in halves");
