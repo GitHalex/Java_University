ee7215eb79662b1a80869b5168726535727eb444
GitHalex <calex1731@gmail.com>
2025-03-28 20:03:51 -0400

    Agregando nuevos archivos a estos caracteres

--- src/capitulo_8/DoubleJeopardy.java
+++ src/capitulo_8/DoubleJeopardy.java
@@ -2,7 +2,15 @@ package capitulo_8;
 
 public class DoubleJeopardy {
     public static void main(String[] args) {
-        short value = 32;
+        int value = 35000;
         System.out.println("A short: " + value);
+
+        double valor = 3;
+        System.out.println("A double: " + valor);
+
+        System.out.println("e to the power value: " + Math.exp(value));
+
+        char ch = 'A';
+        System.out.println("A char: " + ch);
     }
 }
