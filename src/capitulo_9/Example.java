894dffcec496029bd642ec6c4ede336c4a441072
GitHalex <calex1731@gmail.com>
2025-04-01 11:12:10 -0400

    Agregando nuevo package de capitulo 9

---
+++ src/capitulo_9/Example.java
@@ -0,0 +1,8 @@
+package capitulo_9;
+
+public class Example {
+    public static void main(String[] args) {
+        long payAmount = 123;
+        System.out.println("The cariable contains: " + payAmount);
+    }
+}
