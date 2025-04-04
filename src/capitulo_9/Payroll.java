bab708b2ef77b57c6fbf07d2d6eb47c333f09431
GitHalex <calex1731@gmail.com>
2025-04-04 10:26:48 -0400

    Agregando y terminado el ejercicios

--- src/capitulo_9/Payroll.java
+++ src/capitulo_9/Payroll.java
@@ -3,6 +3,14 @@ package capitulo_9;
 public class Payroll {
     public static void main(String[] args) {
         long hoursWorked = 40;
-        double payRate = 10.0, taxRate = 5.0;
+        double payRate = 10.0, taxRate = 0.10;
+        System.out.println("Hours Worked: " + hoursWorked );
+        System.out.println("pay Amount  : " + (hoursWorked * payRate) );
+        System.out.println("tax Amount  : " + (hoursWorked * payRate * taxRate) );
+
+        double x = 0.0;
+        double total = 3*Math.pow(x, 2) - 8*x + 4;
+        System.out.println("Total AT x = " + x + " the value is " + total);
+
     }
 }
