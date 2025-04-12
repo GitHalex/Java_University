35255946584032bb5fe346c3a57b4aa274ab55b8
GitHalex <calex1731@gmail.com>
2025-04-12 15:04:24 -0400

    Agregando nuevos valores

---
+++ src/capitulo_10/Rain.java
@@ -0,0 +1,7 @@
+package capitulo_10;
+
+public class Rain {
+    public static void main(String[] args) {
+        int abril = 12;
+    }
+}
--- src/capitulo_10/RemainderExample.java
+++ src/capitulo_10/RemainderExample.java
@@ -4,11 +4,16 @@ public class RemainderExample {
     public static void main(String[] args) {
 
         int quotient, remainder;
-        quotient = 17 / 3;
-        remainder = 17 % 3;
+        quotient = 17 / -3;
+        remainder = 17 % -3;
 
-        System.out.println("The quotient of " + quotient + " is " + remainder);
-        System.out.println("The remainder of " + remainder + " is " + quotient % 3);
+        System.out.println("The quotient of " + quotient);
+        System.out.println("The remainder of " + remainder);
         System.out.println("The original quotient of " + (quotient*3 + remainder));
+
+        final double DURABLE = 0.045;
+        final double NONDURABLE = 0.038;
+
+
     }
 }
