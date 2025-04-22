d5ce32222dd6d51feed4abdb9121de0fdcf6fcc8
GitHalex <calex1731@gmail.com>
2025-04-21 20:42:52 -0400

    Modificando valores

--- src/capitulo_10/Rain.java
+++ src/capitulo_10/Rain.java
@@ -2,6 +2,14 @@ package capitulo_10;
 
 public class Rain {
     public static void main(String[] args) {
-        int abril = 12;
+        double abril = 12, mayo = 14, june = 8;
+
+        double total = (abril + mayo + june)/3;
+
+        System.out.println("Luvias para el \t" + abril + " de abril");
+        System.out.println("Luvias para el \t" + mayo + " de mayo");
+        System.out.println("Luvias para el \t" + june + " de junio");
+        System.out.println("precipitacion media: " + total);
+
     }
 }
